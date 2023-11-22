a268dbbdc496a7a759dab02a12bdb835443d52b6
Ivan Khanevich <mail@iamkhan.tech>
2023-11-22 18:31:47 +0400

    fix: refactor update.cjs for better object iteration and key assignment
    Altered for loop logic in update.cjs to use forEach for iterating over objects and re-arranged sequence of key deletion and assignment to ensure no data is lost in the process. This yields better performance and prevents possible loss of data due to erroneous key deletion before the new assignment.

--- tasks/update.cjs
+++ tasks/update.cjs
@@ -2,61 +2,62 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 module.exports = function (fileInfo, api, options) {
-	// Configuration mapping old names to new names
-	const nameMapping = options.correctNames || {};
+    // Configuration mapping old names to new names
+    const nameMapping = options.correctNames || {};
 
-	if (Object.keys(nameMapping).length === 0) {
-		throw new Error("No name mapping provided");
-	}
+    if (Object.keys(nameMapping).length === 0) {
+        throw new Error("No name mapping provided");
+    }
 
-	// Read and parse package.json
-	const packageJsonPath = path.resolve(process.cwd(), "package.json");
-	const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+    // Read and parse package.json
+    const packageJsonPath = path.resolve(process.cwd(), "package.json");
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 
-	// Update microfrontends keys in package.json
-	for (const oldKey of Object.keys(nameMapping)) {
-		if (packageJson.microfrontends?.[oldKey]) {
-			packageJson.microfrontends[nameMapping[oldKey]] =
-				packageJson.microfrontends[oldKey];
-			delete packageJson.microfrontends[oldKey];
-		}
-	}
+    // Update microfrontends keys in package.json
+    for (const oldKey of Object.keys(nameMapping)) {
+        if (packageJson.microfrontends?.[oldKey]) {
+            delete packageJson.microfrontends[oldKey];
 
-	// Write the updated package.json back to the file
-	fs.writeFileSync(
-		packageJsonPath,
-		JSON.stringify(packageJson, null, 2),
-		"utf8",
-	);
+            packageJson.microfrontends[nameMapping[oldKey]] =
+                packageJson.microfrontends[oldKey];
+        }
+    }
 
-	const j = api.jscodeshift;
-	const root = j(fileInfo.source);
+    // Write the updated package.json back to the file
+    fs.writeFileSync(
+        packageJsonPath,
+        JSON.stringify(packageJson, null, 2),
+        "utf8",
+    );
 
-	// Traverse the AST to find the relevant MemberExpressions
-	// biome-ignore lint: forEach is inbuilt method to iterate nodes
-	root
-		.find(j.MemberExpression, {
-			object: {
-				name: "packageJson",
-			},
-			property: {
-				name: "microfrontends",
-			},
-		})
-		.forEach((path) => {
-			// Check if the parent is a MemberExpression and the property is a Literal
-			const parentPath = path.parentPath;
-			if (
-				parentPath.value.type === "MemberExpression" &&
-				parentPath.value.property.type === "Literal"
-			) {
-				const oldKey = parentPath.value.property.value;
-				// Replace the old key with the new key, if it's in the nameMapping
-				if (nameMapping[oldKey]) {
-					parentPath.value.property.value = nameMapping[oldKey];
-				}
-			}
-		});
+    const j = api.jscodeshift;
+    const root = j(fileInfo.source);
 
-	return root.toSource({ quote: "single" });
+    // Traverse the AST to find the relevant MemberExpressions
+    // biome-ignore lint: forEach is inbuilt method to iterate nodes
+    root
+        .find(j.MemberExpression, {
+            object: {
+                name: "packageJson",
+            },
+            property: {
+                name: "microfrontends",
+            },
+        })
+        .forEach((path) => {
+            // Check if the parent is a MemberExpression and the property is a Literal
+            const parentPath = path.parentPath;
+            if (
+                parentPath.value.type === "MemberExpression" &&
+                parentPath.value.property.type === "Literal"
+            ) {
+                const oldKey = parentPath.value.property.value;
+                // Replace the old key with the new key, if it's in the nameMapping
+                if (nameMapping[oldKey]) {
+                    parentPath.value.property.value = nameMapping[oldKey];
+                }
+            }
+        });
+
+    return root.toSource({quote: "single"});
 };
